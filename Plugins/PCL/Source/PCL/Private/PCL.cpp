e31fd7687df55aa5cb6b451da63f9ba4a213579d
Sam Richter <hoodie888@outlook.com>
2023-10-30 14:47:23 -0500

    cleanup and test load

--- Plugins/PCL/Source/PCL/Private/PCL.cpp
+++ Plugins/PCL/Source/PCL/Private/PCL.cpp
@@ -2,31 +2,34 @@
 
 #include "PCL.h"
 #include "Misc/MessageDialog.h"
-#include "Modules/ModuleManager.h"
-#include "Interfaces/IPluginManager.h"
-#include "Misc/Paths.h"
-#include "HAL/PlatformProcess.h"
-//#include "PCLLibrary/ExampleLibrary.h"
 
 THIRD_PARTY_INCLUDES_START
 #include <PCL/pcl_config.h>
+#ifdef PCL_TEST_LOAD
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#endif
 THIRD_PARTY_INCLUDES_END
 
 DECLARE_LOG_CATEGORY_EXTERN(PCL, Log, All);
 DEFINE_LOG_CATEGORY(PCL);
 
-void FPCLModule::StartupModule()
-{
-	UE_LOG(PCL, Log, TEXT("################################### \n"));
+
+void FPCLModule::StartupModule() {
 	UE_LOG(PCL, Log, TEXT("Successfully loaded PCL Version: %s \n"), TEXT(PCL_VERSION_PRETTY));
-	UE_LOG(PCL, Log, TEXT("################################### \n"));
+
+#ifdef PCL_TEST_LOAD
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud{ new pcl::PointCloud<pcl::PointXYZ> };
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("C:\\Users\\Hoodi\\Downloads\\Untitled_Scan_15_55_17.pcd", *cloud) == 0) {
+		UE_LOG(PCL, Log, TEXT("Test PCD loaded successfully!: %d points total"), cloud->points.size());
+	} else {
+		UE_LOG(PCL, Log, TEXT("Test PCD load failed :("));
+	}
+#endif
 }
 
-void FPCLModule::ShutdownModule()
-{
-	
-}
+void FPCLModule::ShutdownModule() {}
+
 
 #undef LOCTEXT_NAMESPACE
-	
 IMPLEMENT_MODULE(FPCLModule, PCL)
