8a28543cfa39d7c8254220b5bc9b50c1ecdb24b2
Sam Richter <hoodie888@outlook.com>
2023-10-30 14:17:08 -0500

    Initial commit

---
+++ Source/UESim/UESim.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UESim.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UESim, "UESim" );
